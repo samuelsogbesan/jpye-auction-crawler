e1cb7a00683fb67ec75bfd3ba9f2e1d26912eed7
Samuel Sogbesan <ss3222@bath.ac.uk>
2019-09-29 13:09:30 +0100

    Move Classes
    Moved classes into individual modules

---
+++ Item.py
@@ -0,0 +1,29 @@
+import re
+
+class Item:
+    def __init__(self,data):
+        self.data = data
+        self.lot = ""
+        self.lotNo = ""
+        self.price = ""
+        self.lotTitle = ""
+        self.timeRemaining = ""
+        self.parseData()
+
+    def __str__(self):
+        return  "LOT NO: " + self.lotNo + ", '"+self.lotTitle + "', PRICE £" + self.price + "," + self.timeRemaining + (" remaining" if self.timeRemaining!="Ended" else "")
+
+    def parseData(self):
+        tag= "h5"
+        regex = """(?P<tag>{})(?P<data>.*?)(?P<endTag>/(?P=tag))""".format(tag)
+        dataPoints = re.findall(regex,self.data)
+        dataStrings = []
+        for i in range(0,len(dataPoints)):
+            dataStrings.append("".join(dataPoints[i]).replace(tag+">","").replace("</"+tag,""))
+        try:
+            self.lotNo = dataStrings[0]
+            self.lotTitle = dataStrings[1]
+            self.price = dataStrings[2][7:]
+            self.timeRemaining = dataStrings[3]            
+        except Exception: #If the data is in the wrong format
+            return False
